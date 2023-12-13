b88c081ac7e5f496e57786c751ea66d4d60ed152
Connor Burns <cpburns17@gmail.com>
2023-12-13 09:55:07 -0500

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for n in num_list:
+        if n % 2 == 0:
+            evens.append(n)
+    return evens
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentences = []
+    for s in sentence_list:
+        s = (s + '!')
+        sentences.append(s)
+    return sentences
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
